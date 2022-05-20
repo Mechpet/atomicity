bc9adad69abc2eebf319c375ce306604f1c51292
Mechpet <blazepet2@gmail.com>
2022-05-19 19:26:18 -0500

    Tried scrollArea; have styles

--- dateColumn.py
+++ dateColumn.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QCalendarWidget, QScrollArea
 from PyQt6.QtCore import Qt, QDate
 
 # Maps the dayOfWeek() -> Name of day
@@ -15,23 +15,41 @@ dayNames = {
 class dateColumn(QWidget):
     def __init__(self):
         super().__init__()
-        self.numDays = 7
+        self.numDays = 14
+        self.setStyleSheet("""
+            QLabel#dateLabel {
+                font: 12pt Helvetica;
+                font-weight: bold;
+            }
+            QLabel#dayLabel {
+                font: 10pt Helvetica;
+            }
+        """)
         self.initUI()
 
     def initUI(self):
+        self.scroll = QScrollArea()
         self.topDate = QDate.currentDate()
         self.dates = [self.topDate]
         for i in range(self.numDays):
             self.dates.append(self.dates[-1].addDays(-1))
-        layout = QVBoxLayout()
+        layout = QVBoxLayout(self)
 
         for date in self.dates:
-            # Plan to style each label differently but have them right above/below one another
             dateLabel = QLabel(date.toString(Qt.DateFormat.RFC2822Date), self)
+            dateLabel.setObjectName("dateLabel")
             dayLabel = QLabel(dayNames[date.dayOfWeek()], self)
-            newLabel = QLabel(date.toString(Qt.DateFormat.RFC2822Date) + '\n' + dayNames[date.dayOfWeek()], self)
-            newLabel.setFixedHeight(100)
-            layout.addWidget(newLabel)
+            dayLabel.setObjectName("dayLabel")
+            layout.addWidget(dateLabel)
+            layout.addWidget(dayLabel, 0, Qt.AlignmentFlag.AlignHCenter)
+            layout.addSpacing(50)
 
         self.setLayout(layout)
+
+        #Scroll Area Properties
+        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.scroll.setWidgetResizable(True)
+        #self.scroll.setWidget(self)
+
         self.show()
