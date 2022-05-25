c72c18cce6c53f7b53ba3239c6a11a95fa2d0cac
Mechpet <blazepet2@gmail.com>
2022-05-25 13:42:23 -0500

    Modified layout

--- contentHead.py
+++ contentHead.py
@@ -1,7 +1,7 @@
 # Header of each column (topmost block of data that is static to vertical scrolling)
-from PyQt6.QtWidgets import QWidget, QPushButton, QDialog, QGridLayout, QLabel
+from PyQt6.QtWidgets import QWidget, QPushButton, QDialog, QGridLayout, QVBoxLayout, QLabel, QSpacerItem
 from PyQt6.QtGui import QPainter, QPainterPath, QBrush, QPen, QColor, QTextOption, QCursor, QIcon
-from PyQt6.QtCore import Qt, QRectF, QSize, QSettings, QFile
+from PyQt6.QtCore import Qt, QRectF, QSettings, QSize, QFile
 from settingsWindow import contentHeadSettingsWindow
 
 # # # Attributes:
@@ -37,7 +37,7 @@ class contentHead(QWidget):
         self.layout = QGridLayout()
         # Customize the 'Settings' button
         self.btn = QPushButton("", self)
-        self.btn.resize(QSize(25, 25))
+        self.btn.resize(QSize(75, 75))
         #self.btn.move(self.size * 0.05, self.size * 0.85)
         self.btn.setToolTip("<b>Settings</b>")
         self.btn.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
@@ -46,6 +46,8 @@ class contentHead(QWidget):
             QPushButton {
                 border: 0px;
                 background: transparent;
+                width: 20px;
+                height: 20px;
                 border-image: url(images/appIcons/cogwheel_idle.png);
             }
             QPushButton:hover {
@@ -53,11 +55,13 @@ class contentHead(QWidget):
             }
         """)
         self.btn.clicked.connect(self.settingsWindow)
-        self.iconBtn = QPushButton("Icon", self)
+        self.iconBtn = QPushButton("", self)
+        self.iconBtn.resize(QSize(75, 75))
         self.iconBtn.setStyleSheet("""
             QPushButton {
                 border: 0px;
                 background: transparent;
+                qproperty-iconSize: 50px;
             }
         """)
         if self.iconPath:
@@ -76,11 +80,19 @@ class contentHead(QWidget):
                 self.alert.setLayout(layout)
                 self.alert.exec()
         
-        self.layout.addWidget(self.btn, 0, 0)
-        self.layout.addWidget(self.iconBtn, 0, 1)
+        
+        for i in range(10):
+            self.layout.setColumnStretch(i, 1)
+        self.layout.addWidget(self.iconBtn, 0, 0, 3, 3)
+        for i in range(10):
+            print(f"Column {i} stretch: {self.layout.columnStretch(i)}")
+            print(f"Column {i} minWidth: {self.layout.columnMinimumWidth(i)}")
+        self.layout.addWidget(self.btn, 0, 9, 1, 1)
+        print(f"Num columns = {self.layout.columnCount()}")
+        print(f"Num rows = {self.layout.rowCount()}")
         self.setLayout(self.layout)
+        
 
-        print("SUP")
 
 
     def paintEvent(self, e):
